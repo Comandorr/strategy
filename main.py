48a096e40431119b613dfb5a7a61e20aee8e55ae
creazerodota@gmail.com <creazerodota@gmail.com>
2017-02-22 22:09:08 +0300

    Fix help menu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-#main
+# main
 # Прочитай ссылку про PEP8, реально
 import os
+
 
 class City:
     def __init__(self, name, lvl, exp, hp, units):
@@ -9,6 +10,7 @@ class City:
         self.exp = exp
         self.units = units
         self.hp = hp
+
 
 class Unit:
     def __init__(self, name, city, cost, hp, attack, active):
@@ -19,6 +21,7 @@ class Unit:
         self.hp = hp
         self.attack = attack
 
+
 gold = 100
 turns = 1
 cities_l = []
@@ -28,20 +31,27 @@ units_l.append(Unit('Soldier', '', 30, 30, 30, True))
 units_l.append(Unit('Badass Soldier', '', 50, 65, 65, True))
 units_l.append(Unit('REALLY BADASS SOLDIER', '', 75, 90, 90, True))
 
+
 def commander():
-	command = input("INPUT ")
-	if command == "exit": ()
-	elif command == 'create unit': create_unit_v2()
-	elif command == 'create city':
-		if len(command.split(' ')) == 3: create_city(command.split(' ')[2])
-		else:
-			print('INVALID COMMAND')	
-			commander()
-	elif command == "turn": turn()
-	elif command == 'help': help()
-	else:
-		print('INVALID COMMAND')
-		commander()
+    command = input("INPUT ")
+    if command == "exit":
+        exit(1)
+    elif command == 'create unit':
+        create_unit()
+    elif command == 'create city':
+        if len(command.split(' ')) == 3:
+            create_city(command.split(' ')[2])
+        else:
+            print('INVALID COMMAND')
+            commander()
+    elif command == "turn":
+        turn()
+    elif command == 'help':
+        help_command()
+    else:
+        print('INVALID COMMAND')
+        commander()
+
 
 def cities():
     os.system("clear")
@@ -49,50 +59,33 @@ def cities():
         city.units = 0
         for unit in units_l:
             if city.name == unit.city:
-                city.units +=1
-
-    """
-    Этот цикл можно заменить на:
-    for number, value in enumerate(cities_l):
-        ...
-    Про enumerate() прочитай в доках
-    """
-    for i in range(len(cities_l)):
-        city = cities_l[i]
-        print("{n}.  CITY {name} | LVL {lvl} | EXP {exp} OF {exp1} | HP {hp} OF {hp1} | UNITS {units}".format(n=i+1,
-                                                                                           name = city.name,
-                                                                                           lvl=city.lvl,
-                                                                                           exp=city.exp,
-                                                                                           exp1=city.lvl*150,
-                                                                                           hp = city.hp,
-                                                                                           hp1 = 300+(city.lvl-1)*100,
-                                                                                           units = city.units))
+                city.units += 1
+    for i, city in enumerate(cities_l):
+        print(("{0}.  CITY {1} | LVL"
+               "{2} | EXP {3} OF {4} |"
+               "HP {5} OF {6} | UNITS {7}").format(i + 1,
+                                                   city.name,
+                                                   city.lvl,
+                                                   city.exp,
+                                                   city.lvl * 150,
+                                                   city.hp,
+                                                   (city.lvl - 1) * 100 + 300,
+                                                   city.units))
         print("----------------------------------------------------------------------------")
-    print("GOLD {gold} | TURN {turn}".format(gold = gold, turn = turns))
+    print("GOLD {0} | TURN {1}".format(gold, turns))
     commander()
 
-def create_city(name):
-	global gold
-	if gold >= 300:
-		cities_l.append(City(name, 1, 0, 300, 0))
-		gold -= 300	
-	else:
-		print('NOT ENOUGH GOLD (300 NEED)')
-		commander()
-	cities()
 
-def create_unit(n):
+def create_city(name):
     global gold
-    if gold < units_l[0].cost*n:
-        print('NOT ENOUGH GOLD ({cost} NEED)'.format(cost = 30*n))
-        commander()
+    if gold >= 300:
+        cities_l.append(City(name, 1, 0, 300, 0))
+        gold -= 300
     else:
-        for i in range(n):
-            if gold >= units_l[0].cost:
-                units_l.append(Unit('Soldier','Capital', 30, True))
-                gold -= units_l[0].cost
-            else: break
+        print('NOT ENOUGH GOLD (300 NEED)')
+        commander()
     cities()
+
 
 def turn():
     global gold
@@ -101,44 +94,57 @@ def turn():
     turns += 1
     for city in cities_l:
         city.exp += 30
-        if city.exp >= city.lvl*150:
-            city.exp -= city.lvl*150
+        if city.exp >= city.lvl * 150:
+            city.exp -= city.lvl * 150
             city.lvl += 1
     cities()
 
-def help():
-	os.system('clear')
-	print('HELP')
-	print('create city "name"')
-	print('create unit + nomer & kolichestvo')
-	print('turn')
-	input('exit')
-	cities()
 
-def create_unit_v2():
-	global gold
-	print('1.  SOLDIER | HP {hp} | ATTACK {atk} | COST {cost}'.format(hp = units_l[0].hp,
-															atk = units_l[0].attack,
-															cost = units_l[0].cost))
-	print('2.  BADASS SOLDIER | HP {hp} | ATTACK {atk} | COST {cost}'.format(hp = units_l[1].hp,
-															atk = units_l[1].attack,
-															cost = units_l[1].cost))
-	print('3.  REALLY BADASS SOLDIER | HP {hp} | ATTACK {atk} | COST {cost}'.format(hp = units_l[2].hp,
-															atk = units_l[2].attack,
-															cost = units_l[2].cost))
-	command = input()
-	u = int(command.split(' ')[0])-1   #unit type
-	if len(command.split(' ')) == 2:		   #number
-		n = int(command.split(' ')[1])
-	elif len(command.split(' ')) == 1: n = 1
-	if gold < units_l[u].cost*n:
-		input('NOT ENOUGH GOLD ({cost} NEED)'.format(cost = units_l[u].cost*n))
-		cities()
-	else:
-		for i in range(n):
-				if gold >= units_l[u].cost:
-					units_l.append(Unit(units_l[u].name, 'Capital', units_l[u].cost, units_l[u].hp, units_l[u].attack, True))
-					gold -= units_l[u].cost
-				else: break
-	cities()
-help()
+def help_command():
+    print('\nHELP')
+    print('create city [CITY NAME] - create new city')
+    print('create unit [UNIT NUMBER] [NUMBER OF UNITS] - create one or more units in city')
+    print('turn - next turn')
+    input("press any button to close help...")
+    cities()
+
+
+def create_unit():
+    global gold
+    print('1.  SOLDIER | HP {0} | ATTACK {1} | COST {2}'.format(units_l[0].hp,
+                                                                units_l[0].attack,
+                                                                units_l[0].cost))
+    print('2.  BADASS SOLDIER | HP {0} | ATTACK {1} | COST {2}'.format(units_l[1].hp,
+                                                                       units_l[1].attack,
+                                                                       units_l[1].cost))
+    print('3.  REALLY BADASS SOLDIER | HP {0} | ATTACK {1} | COST {2}'.format(units_l[2].hp,
+                                                                              units_l[2].attack,
+                                                                              units_l[2].cost))
+    print('CREATE UNIT [UNIT NUMBER] [NUMBER OF UNITS]', end='')
+    command = input()
+    unit_type = int(command.split(' ')[0]) - 1  # unit type
+    if len(command.split(' ')) == 2:  # number
+        n = int(command.split(' ')[1])
+    elif len(command.split(' ')) > 2:
+        print('INVALID COMMAND')
+        create_unit()
+        return
+    else:
+        n = 1
+    if gold < units_l[unit_type].cost * n:
+        input('NOT ENOUGH GOLD ({cost} NEED)'.format(cost=units_l[unit_type].cost * n))
+        cities()
+    else:
+        for i in range(n):
+            if gold >= units_l[unit_type].cost:
+                units_l.append(
+                    Unit(units_l[unit_type].name, 'Capital',
+                         units_l[unit_type].cost, units_l[unit_type].hp,
+                         units_l[unit_type].attack, True))
+                gold -= units_l[unit_type].cost
+            else:
+                break
+    cities()
+
+
+cities()
