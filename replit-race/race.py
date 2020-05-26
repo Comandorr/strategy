e5f3697b2c7a44dac21400dff65c65344289f2f0
Comandorr <Plane135790@gmail.com>
2020-05-26 23:56:57 +0300

    unimportant codestyle changes

--- replit-race/race.py
+++ replit-race/race.py
@@ -1,5 +1,4 @@
-import play
-import math
+import play, math
 
 
 player = play.new_image(image = "car.png", size = 10)
@@ -8,6 +7,7 @@ speedometer = play.new_text(words = str( int( abs(player.physics.x_speed) + abs(
                             x = -350, y = 250, font_size = 40)
 player.ang = 0
 player.speed = 0
+player.max_speed = 30
 player.vector = [0, 0]
 
 box1 = play.new_box(x = -200)
@@ -26,27 +26,27 @@ async def do():
     player.vector[0] = math.cos(math.radians(-player.angle))
     player.vector[1] = math.sin(math.radians(-player.angle))
 
-    if play.key_is_pressed('w', 'ц') and player.speed < 30:         # acceleration and back
+    if play.key_is_pressed('w', 'ц') and player.speed < player.max_speed:  # acceleration and back
         player.speed += 1
-    if play.key_is_pressed('s', 'ы') and player.speed > -30:
+    if play.key_is_pressed('s', 'ы') and player.speed > -player.max_speed:
         player.speed -= 1
 
-    if play.key_is_pressed('a', 'ф'):                        # turns
+    if play.key_is_pressed('a', 'ф'):                                      # turns
         player.turn(0.1 * abs(player.speed))
     if play.key_is_pressed('d', 'в'):
         player.turn(-0.1 * abs(player.speed))
     player.ang = player.angle
 
-    brake = 0                                               # handbrake 1/2
+    brake = 0                                                              # handbrake 1/2
     if play.key_is_pressed("space"):
         brake = 1.5
 
-    if player.speed > 0:                                           # slow down
+    if player.speed > 0:                                                   # slow down
         player.speed -= 0.2 + brake
     elif player.speed < 0:
         player.speed += 0.2 + brake
 
-    if play.key_is_pressed("space"):                        # handbrake 2/2
+    if play.key_is_pressed("space"):                                       # handbrake 2/2
         player.speed *= 0.99
         if abs(player.speed) < 0.2:
             player.speed = 0
